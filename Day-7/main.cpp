1a2fa7b3c163e1bd51371feb4e128e8c133ddbc8
1000len-6578 <pschaetzle@testo.de>
2022-12-07 16:28:49 +0100

    feat: support bigger filesystems

--- Day-7/main.cpp
+++ Day-7/main.cpp
@@ -5,11 +5,13 @@
 #include <unordered_map>
 #include "../Utils/util.h"
 
-std::vector<int> mSizes;
+#define LIMIT 700000000
+#define DISK 3000000000
+std::vector<size_t> mSizes;
 
 class Node {
 public:
-  virtual int getSize() = 0;
+  virtual size_t getSize() = 0;
   virtual std::shared_ptr<Node> getParent() = 0;
   virtual std::shared_ptr<Node> getSubdir(const std::string& name) = 0;
   virtual void add(const std::string& name, const std::shared_ptr<Node>& sptrNode) = 0;
@@ -19,8 +21,8 @@ class Dir : public Node {
 public:
   Dir() {}
   Dir(const std::shared_ptr<Node>& sptrParent) : m_sptrParent(sptrParent) {}
-  int getSize() override {
-    int result = 0;
+  size_t getSize() override {
+    size_t result = 0;
     for (const std::pair<std::string, std::shared_ptr<Node>> &n : m_subdirs) {
       result += n.second->getSize();
     }
@@ -44,20 +46,20 @@ private:
   std::unordered_map<std::string, std::shared_ptr<Node>> m_subdirs;
   std::string m_name;
   std::shared_ptr<Node> m_sptrParent;
-  int otherSize = 0;
+  size_t otherSize = 0;
 };
 
 class File : public Node {
 public:
-  File(int size) : m_size(size) {}
-  int getSize() override {
+  File(size_t size) : m_size(size) {}
+  size_t getSize() override {
     return m_size;
   }
   std::shared_ptr<Node> getParent() override {;}
   std::shared_ptr<Node> getSubdir(const std::string& name) override {;}
   void add(const std::string& name, const std::shared_ptr<Node>& sptrNode) override{;}
 private:
-  int m_size;
+  size_t m_size;
 };
 
 bool isCommand(const std::string& strLine) {
@@ -72,7 +74,7 @@ int main(int argc, char *argv[]) {
   std::ifstream inputFile;
   inputFile.open("inputs.txt");
 
-  std::vector<int> sizes;
+  std::vector<size_t> sizes;
   std::string strLine;
   std::vector<std::string> input; 
   std::shared_ptr<Node> currentDir;
@@ -94,24 +96,24 @@ int main(int argc, char *argv[]) {
       if(isDir(strLine)) {
 	currentDir->add(input.back(), std::make_shared<Dir>(currentDir));
       } else {
-	currentDir->add(input.back(), std::make_shared<File>(std::stoi(input.front())));
+	currentDir->add(input.back(), std::make_shared<File>(std::stol(input.front())));
       }
     }
   }
 
-  int filesystemSize = rootDir->getSize();
+  size_t filesystemSize = rootDir->getSize();
 
-  int size = 0;
+  size_t size = 0;
   std::sort(mSizes.begin(), mSizes.end());
-  for(int i : mSizes) {
+  for(size_t i : mSizes) {
     if(i <= 100000)
       size += i; 
   }
 
   std::cout << "Part 1: " << size << std::endl;
 
-  for(int i : mSizes) {
-    if(i > (filesystemSize - 40000000)) {
+  for(size_t i : mSizes) {
+    if(i > (filesystemSize - (DISK - LIMIT))) {
       std::cout << "Part 2: " << i << std::endl;
       break;
     }
